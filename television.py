5a42876ea298b13cd9c62dd4c8d93ac3b8bedae8
bstillmanUNO <111910420+bstillmanUNO@users.noreply.github.com>
2024-04-19 18:58:53 -0500

    Update television.py to contain docstrings

--- television.py
+++ television.py
@@ -5,19 +5,23 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
+        """Initializes the television to default"""
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
     def power(self):
+        """Toggles the power status of the television"""
         self.__status = not self.__status
 
     def mute(self):
+        """Toggles mute for the television"""
         if self.__status:
             self.__muted = not self.__muted
 
     def channel_up(self):
+        """Increases the channel number of the television"""
         if self.__status:
             if self.__channel == Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
@@ -25,6 +29,7 @@ class Television:
                 self.__channel += 1
 
     def channel_down(self):
+        """Decreases the channel number of the television"""
         if self.__status:
             if self.__channel == Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
@@ -32,12 +37,14 @@ class Television:
                 self.__channel -= 1
 
     def volume_up(self):
+        """Increases the volume of the television"""
         if self.__status:
             self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
     def volume_down(self):
+        """Decreases the volume of the television"""
         if self.__status:
             self.__muted = False
             if self.__volume > Television.MIN_VOLUME:
